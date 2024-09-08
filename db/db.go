ec26fd4f5e0f4258f0a8eef780c23cd4e1c6370b
madsbyriel <madshbyriel@gmail.com>
2024-09-08 16:05:39 +0200

    Created simple interface

---
+++ db/db.go
@@ -0,0 +1,10 @@
+package db
+
+import "database/sql"
+
+// Interface for simple sql database connection
+type DB interface {
+    QueryRow(query string, args ...any) (*sql.Row, error)
+    QueryRows(query string, args ...any) (*sql.Rows, error)
+    Execute(query string, args ...any) (*sql.Result, error)
+}
